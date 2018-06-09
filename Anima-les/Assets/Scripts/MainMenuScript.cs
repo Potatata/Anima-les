9ba1ab67cc1e1511c90eebb796bb9506b99333d6
Sivana <sivanahamer@gmail.com>
2018-06-09 09:19:23 -0600

    Added functionality to main menu

---
+++ Anima-les/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    private const int MAINMENUINDEX = 0;
+    private const int CREDITINDEX = 1;
+    private const int PLAYINDEX = 2;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(PLAYINDEX);
+    }
+
+    public void CreditsGame()
+    {
+        SceneManager.LoadScene(CREDITINDEX);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

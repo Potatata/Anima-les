e100fd2cb0aec3e02c0b797583f1edba6749e0d9
Sivana <sivanahamer@gmail.com>
2018-06-09 05:12:49 -0600

    Added a timer and a gitIgnore

--- Anima-les/Assets/Scripts/Timer.cs
+++ Anima-les/Assets/Scripts/Timer.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour {
 
+    float currentTime;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        currentTime = 30.0f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        currentTime -= Time.deltaTime;
+        if(!(currentTime > 0) )
+        {
+            //Show end game screen death
+        }
 	}
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20, 50, 20, 40), ""+ currentTime.ToString("0"));
+    }
 }

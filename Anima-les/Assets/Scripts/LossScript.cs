284da84f576dad05baee3012c82acb3ccf2ac1ad
Sivana <sivanahamer@gmail.com>
2018-06-09 22:52:02 -0600

    Update graphics

---
+++ Anima-les/Assets/Scripts/LossScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LossScript : MonoBehaviour {
+    private const int MAINMENUINDEX = 0;
+    private const int PLAYINDEX = 2;
+
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(MAINMENUINDEX);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(PLAYINDEX);
+    }
+
+}
--- Anima-les/Assets/Scripts/WinningScript.cs
+++ Anima-les/Assets/Scripts/WinningScript.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinningScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private const int MAINMENUINDEX = 0;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Wait());
+        SceneManager.LoadScene(MAINMENUINDEX);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10.0f);
+    }
 }

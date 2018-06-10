1e3acad9f7c0eb82b6a2315b9f97366aeb874f5c
Sivana <sivanahamer@gmail.com>
2018-06-09 20:50:31 -0600

    Added art files and implementation

---
+++ Anima-les/Assets/Scripts/BegginingSceneScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BegginingSceneScript : MonoBehaviour {
+
+    private const int MAINMENUINDEX = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(Wait());
+        SceneManager.LoadScene(MAINMENUINDEX);
+
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10.0f);
+    }
+}
